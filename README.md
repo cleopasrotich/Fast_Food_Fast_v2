1534217be9f09a866955cd8088a4f4f313784fab
cleopasrotich <cleopasrotich81@gmail.com>
2018-09-24 12:47:10 +0300

    my version 2 repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fast Food Fast APIs
+
+Contains all APIs for the Fast food fast web application.
+
+## Screenshots of the API test on Postman.
+
+![GEt_all](pics/Get_all.png)
+
+![GEt_one](pics/get_one.png)
---
+++ run.py
@@ -0,0 +1,14 @@
+import os
+
+from app import create_app
+#from app import orders
+
+
+config = os.getenv('APP_SETTINGS')
+app = create_app('development')
+
+from app.api.v1.views import *
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
