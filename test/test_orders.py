212b07d426e84a024b48b76158a3aae6243319c9
cleopasrotich <cleopasrotich81@gmail.com>
2018-09-25 11:45:59 +0300

    my coveralls

--- test/test_orders.py
+++ test/test_orders.py
@@ -4,28 +4,28 @@ import unittest
 app = Flask(__name__)
 
 
-class Testendpoints(unittest.TestCase):
+class Testorders(unittest.TestCase):
     def test_all(self):
-        result=app.test_client()
-        response= result.get('/fast_foods/api/v1/orders',content_type='application/json')
+        result = app.test_client()
+        response = result.get('/fast_foods/api/v1/orders',content_type='application/json')
         self.assertTrue(response.status_code, 200)
 
     def test_all_orders(self):
         result = app.test_client()
-        response= result.get('/fast_foods/api/v1/orders', content_type='application/json')
+        response = result.get('/fast_foods/api/v1/orders', content_type='application/json')
         self.assertTrue(response.status_code, 200)
 
     def test_edit_order(self):
         result = app.test_client()
-        response= result.post('/fast_foods/api/v1/orders',  content_type='application/json')
+        response = result.post('/fast_foods/api/v1/orders',  content_type='application/json')
         self.assertTrue(response.status_code, 200)
 
     def test_update_order(self):
         result = app.test_client()
-        response= result.get('/fast_foods/api/v1/orders/2', content_type='application/json')
+        response = result.get('/fast_foods/api/v1/orders/2', content_type='application/json')
         self.assertTrue(response.status_code, 200)
 
     def test_remove_order(self):
         result = app.test_client()
-        response= result.get('/fast_foods/api/v1/orders/2', content_type='application/json')
+        response = result.get('/fast_foods/api/v1/orders/2', content_type='application/json')
         self.assertTrue(response.status_code, 200)
