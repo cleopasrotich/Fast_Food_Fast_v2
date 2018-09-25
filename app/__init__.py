ea9db2cabc334372cd3035812b69d040fccb89c8
cleopasrotich <cleopasrotich81@gmail.com>
2018-09-25 13:11:36 +0300

    my init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 def create_app(config_name):
 
     app = Flask(__name__)
+
+
+
+    @app.errorhandler(404)
+    def error1(error):
+        return jsonify({"Page not found"}), 404
+
+    @app.errorhandler(500)
+    def error_2(error):
+        return jsonify({"There seem to be a problem with your server."}), 404
     return app
